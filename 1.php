190ba42553c8bdbbceb11afc0e032b488151f067
Anasvirat <anasmohammed18745@gmail.com>
2025-05-27 10:46:14 +0530

    Update 1.php

--- 1.php
+++ 1.php
@@ -64,7 +64,7 @@ if (empty($id)) {
         fclose($fp);
     } else {
         session_write_close(); // Unlock the session data
-        $url_for_domain = "http://starshare.org/live/edwinnn/edwinnn/{$id}.ts";
+        $url_for_domain = "http://tigerott.com:8080/live/0583333/111111/{$id}.ts";
         $domain = extractDomain($url_for_domain, $headers);
 
         if ($domain) {
